2228dc9b47bf71a7e521e0e0ba537a91b270a74f
luoluo <1425735414@qq.com>
2024-07-02 17:49:27 +0800

    测试打包-35

--- buildHooks/after_pack.js
+++ buildHooks/after_pack.js
@@ -1,5 +1,5 @@
 module.exports = async function (context) {
-    console.log('arch', context.arch, arch)
+    console.log('arch', context.arch)
     console.log('electronPlatformName', context.electronPlatformName)
     console.log('context.packager.config.linux', context.packager.config.linux)
     console.log('context.packager.config.mac', context.packager.config.mac)
