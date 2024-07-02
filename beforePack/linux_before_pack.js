fb0aba02cc7b12b2c8bfa90614976dac8a9d6ed0
luoluo <1425735414@qq.com>
2024-07-02 14:16:35 +0800

    测试打包-23

--- beforePack/linux_before_pack.js
+++ beforePack/linux_before_pack.js
@@ -2,38 +2,29 @@ module.exports = async function (context) {
     const arch = context.arch;
     const config = context.electronPlatformName === 'linux' ? context.packager.config.linux : null;
     if (config) {
-        if (arch === 'x64') {
-            config.artifactName = '${productName}-${version}-linux-amd64.${ext}';
-            config.extraFiles = [
-                {
-                    "from": "bins/flag.linux.txt",
-                    "to": "bins/flag.linux.txt",
-                },
-                {
-                    "from": "bins/yak_linux_amd64.zip",
-                    "to": "bins/yak.zip",
-                },
-                {
-                    "from": "report/template.zip",
-                    "to": "report/template.zip",
-                },
-            ];
-        } else if (arch === 'arm64') {
-            config.artifactName = '${productName}-${version}-linux-arm64.${ext}';
-            config.extraFiles = [
-                {
-                    "from": "bins/flag.linux.txt",
-                    "to": "bins/flag.linux.txt",
-                },
-                {
-                    "from": "bins/yak_linux_arm64.zip",
-                    "to": "bins/yak.zip",
-                },
-                {
-                    "from": "report/template.zip",
-                    "to": "report/template.zip",
-                },
-            ];
+        switch (arch) {
+            case 'x64':
+                config.artifactName = '${productName}-${version}-linux-amd64.${ext}';
+                config.extraFiles = [
+                    ...config.extraFiles,
+                    {
+                        "from": "bins/yak_linux_amd64.zip",
+                        "to": "bins/yak.zip",
+                    },
+                ]
+                break;
+            case 'arm64':
+                config.artifactName = '${productName}-${version}-linux-arm64.${ext}';
+                config.extraFiles = [
+                    ...config.extraFiles,
+                    {
+                        "from": "bins/yak_linux_arm64.zip",
+                        "to": "bins/yak.zip",
+                    },
+                ]
+                break;
+            default:
+                break;
         }
     }
 };
