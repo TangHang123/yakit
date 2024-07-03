a80380ed975589f67d492588fd9560dadbca41d2
luoluo <1425735414@qq.com>
2024-07-03 14:16:06 +0800

    测试打包-40

--- buildHooks/before_pack.js
+++ buildHooks/before_pack.js
@@ -48,37 +48,38 @@ module.exports = async function (context) {
     }
     /**mac */
     /** 1:x64 3:arm64 */
-    // const macConfig = context.electronPlatformName === 'darwin' ? context.packager.config.mac : null;
-    // if (macConfig) {
-    //     const darwinExtraFiles = [
-    //         {
-    //             "from": "bins/flag.darwin.txt",
-    //             "to": "bins/flag.darwin.txt",
-    //         },
-    //     ]
-    //     switch (arch) {
-    //         case 'x64':
-    //             macConfig.extraFiles = [
-    //                 ...darwinExtraFiles,
-    //                 {
-    //                     "from": "bins/yak_darwin_amd64.zip",
-    //                     "to": "bins/yak.zip",
-    //                 },
-    //             ]
-    //             break;
-    //         case 'arm64':
-    //             macConfig.extraFiles = [
-    //                 ...darwinExtraFiles,
-    //                 {
-    //                     "from": "bins/yak_darwin_arm64.zip",
-    //                     "to": "bins/yak.zip",
-    //                 },
-    //             ]
-    //             break;
-    //         default:
-    //             break;
-    //     }
-    //     console.log('macConfig', macConfig.artifactName, macConfig.extraFiles)
-    //     context.packager.config.mac = { ...macConfig }
-    // }
+    const macConfig = context.electronPlatformName === 'darwin' ? context.packager.config.mac : null;
+    if (macConfig) {
+        const darwinExtraFiles = [
+            {
+                "from": "bins/flag.darwin.txt",
+                "to": "bins/flag.darwin.txt",
+            },
+        ]
+        switch (arch) {
+            case 'arm64':
+                macConfig.extraFiles = [
+                    ...darwinExtraFiles,
+                    {
+                        "from": "bins/yak_darwin_arm64.zip",
+                        "to": "bins/yak.zip",
+                    },
+                ]
+                break;
+            case 'x64':
+                macConfig.extraFiles = [
+                    ...darwinExtraFiles,
+                    {
+                        "from": "bins/yak_darwin_amd64.zip",
+                        "to": "bins/yak.zip",
+                    },
+                ]
+                break;
+            
+            default:
+                break;
+        }
+        console.log('macConfig', macConfig.artifactName, macConfig.extraFiles)
+        context.packager.config.mac = { ...macConfig }
+    }
 };
